8a316e5011eb363a249323cc78df9150c3b7477f
Misaka desu <24862715+funct1onal@users.noreply.github.com>
2018-06-02 00:24:11 -0700

    Update emoji.hs
    typo

--- emoji.hs
+++ emoji.hs
@@ -8,6 +8,6 @@ main = return()
 (🔥) _ _ [] = [] 
 (🔥) f (x:xs) (y:ys) = f x y : (🔥) f xs ys
 (😈) = (\ (_:xs) -> xs) -- lambda tail 
-(😂) :: [Integer]  -- thinking face is fib
+(😂) :: [Integer]  -- laughing face is fib
 (💪) = (+)
 (😂) = 0 : 1 : (🔥) (💪) (😂) ((😈) (😂))
